88dcbb24036b3c5481e2ff8f07385dba672f4bdb
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-08-15 16:24:40 +0200

    project 1 page

---
+++ w3/projects.js
@@ -0,0 +1,33 @@
+const root = document.querySelector('#root')
+
+const githubUrl = (githubRef) => `https://github.com/${githubRef}/`
+
+const getProjectUrl = (student) => {
+  const githubProject = `${student.github.username}/${student.projects[0]}`
+  const url = githubUrl(githubProject)
+  return url
+}
+
+fetch('../students.json')
+  .then((res) => res.json())
+  .then((info) => {
+    const getActiveStudents = () => {
+      return info.filter((x) => x.status !== 'deferred')
+    }
+
+    const ul = document.createElement('ul')
+
+    getActiveStudents().forEach((student) => {
+      console.log(student)
+      const li = document.createElement('li')
+      li.innerHTML = `<div>
+      <h2>${student.name}</h2>
+      <a href="${getProjectUrl(student)}">
+        ${student.projects[0]}
+      </a>
+      </div>`
+      ul.appendChild(li)
+    })
+
+    root.append(ul)
+  })
