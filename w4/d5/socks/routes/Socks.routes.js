b24f260440bc9d58e25230173a7a252f5bc646b4
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-08-19 15:22:13 +0200

    adding get by id, patch, delete routes

--- w4/d5/socks/routes/Socks.routes.js
+++ w4/d5/socks/routes/Socks.routes.js
@@ -9,6 +9,12 @@ router.get('/', async (req, res) => {
   res.json({ socks })
 })
 
+// GET /socks/:id
+router.get('/:id', async (req, res) => {
+  const sock = await Sock.findById(req.params.id)
+  res.json({ sock })
+})
+
 // POST /socks
 router.post('/', async (req, res) => {
   const newSock = req.body
@@ -20,6 +26,27 @@ router.post('/', async (req, res) => {
   console.log('createdSock', createdSock)
 
   res.json({ sock: createdSock })
+})
+
+// PATCH /socks/:id
+router.patch('/:id', async (req, res) => {
+  const newSock = req.body
+  const sock = await Sock.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: newSock.name,
+      colour: newSock.colour,
+      foot: newSock.foot,
+    },
+    { new: true }
+  )
+  res.json({ sock })
+})
+
+// DELETE /socks/:id
+router.delete('/:id', async (req, res) => {
+  await Sock.findByIdAndDelete(req.params.id)
+  res.sendStatus(204)
 })
 
 // export router so we can import it elsewhere
