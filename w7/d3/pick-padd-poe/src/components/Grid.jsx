b84efef4bf641fe4037e69feda1350648c488289
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-06 15:36:05 +0200

    grid

--- w7/d3/pick-padd-poe/src/components/Grid.jsx
+++ w7/d3/pick-padd-poe/src/components/Grid.jsx
@@ -1,12 +1,28 @@
+import { useState } from 'react'
 import './Grid.css'
 
 import GridSquare from './GridSquare'
 
 const Grid = ({ width = 3, height = 3 }) => {
+  const [currentPlayer, setCurrentPlayer] = useState(0)
+
+  const advanceToNextPlayer = () => {
+    if (currentPlayer === 'padd') {
+      setCurrentPlayer('poe')
+    } else {
+      setCurrentPlayer('padd')
+    }
+  }
+
   const cells = []
   for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
-      cells.push(<GridSquare />)
+      cells.push(
+        <GridSquare
+          advanceToNextPlayer={advanceToNextPlayer}
+          currentPlayer={currentPlayer}
+        />
+      )
     }
   }
 
--- w7/d3/pick-padd-poe/src/components/GridSquare.jsx
+++ w7/d3/pick-padd-poe/src/components/GridSquare.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
 import './GridSquare.css'
 
-const GridSquare = () => {
-  return <div className="GridSquare"></div>
+const GridSquare = ({ currentPlayer = 'padd' }) => {
+  const [player, setPlayer] = useState('')
+  const classNames = ['GridSquare']
+
+  if (player) {
+    classNames.push(player)
+  }
+
+  const handleClick = () => {
+    if (player) {
+      // no need to set a player if we already have one
+      return
+    }
+
+    setPlayer(currentPlayer)
+  }
+
+  return <div onClick={handleClick} className={classNames.join(' ')}></div>
 }
 
 export default GridSquare
