2574ba7eb3965620aa478048d1d203ad01b88f34
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-07 11:09:44 +0200

    grid lifting state up livecoding

--- w7/d3/pick-padd-poe/src/components/Grid.jsx
+++ w7/d3/pick-padd-poe/src/components/Grid.jsx
@@ -1,14 +1,61 @@
+import { useState } from 'react'
 import './Grid.css'
 
 import GridSquare from './GridSquare'
 
 const Grid = ({ width = 3, height = 3 }) => {
+  const [currentPlayer, setCurrentPlayer] = useState('padd')
+  const [playerSquares, setPlayerSquares] = useState([
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ])
+
+  const toggleCurrentPlayer = () => {
+    setCurrentPlayer(currentPlayer === 'padd' ? 'poe' : 'padd')
+  }
+
+  const checkForWinner = (squares) => {
+    if (
+      squares[0] !== '' &&
+      squares[0] === squares[1] &&
+      squares[0] === squares[2]
+    ) {
+      alert(`player ${squares[0]} has won`)
+    }
+  }
+
   const cells = []
   for (let i = 0; i < height; i++) {
     for (let j = 0; j < width; j++) {
-      cells.push(<GridSquare />)
+      const index = i * width + j
+
+      const setPlayer = () => {
+        const newPlayerSquares = [...playerSquares]
+        newPlayerSquares[index] = currentPlayer
+        setPlayerSquares(newPlayerSquares)
+        checkForWinner(playerSquares)
+      }
+
+      cells.push(
+        <GridSquare
+          key={index}
+          player={playerSquares[index]}
+          setPlayer={setPlayer}
+          currentPlayer={currentPlayer}
+          toggleCurrentPlayer={toggleCurrentPlayer}
+        />
+      )
     }
   }
+
+  console.log(cells)
 
   return <div className="Grid">{cells}</div>
 }
--- w7/d3/pick-padd-poe/src/components/GridSquare.jsx
+++ w7/d3/pick-padd-poe/src/components/GridSquare.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react'
 import './GridSquare.css'
 
-const GridSquare = () => {
-  return <div className="GridSquare"></div>
+const GridSquare = ({
+  player,
+  setPlayer,
+  currentPlayer = 'padd',
+  toggleCurrentPlayer,
+}) => {
+  const handleClick = () => {
+    if (player !== '') {
+      // if player is already set to a real value
+      // we don't need to do anything for this click
+      return
+    }
+
+    // When clicking on a GridSquare, I want to see
+    // the current player's icon in the square
+    setPlayer(currentPlayer)
+
+    // we need to switch the currentPlayer
+    toggleCurrentPlayer()
+  }
+
+  return <div className={`GridSquare ${player}`} onClick={handleClick}></div>
 }
 
 export default GridSquare
