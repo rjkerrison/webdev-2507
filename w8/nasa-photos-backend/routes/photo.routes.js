6d30fa64f3b4c8d37e2c29c8c91531ae8cb28bd9
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-09 18:08:59 +0200

    adding review get and post routes

--- w8/nasa-photos-backend/routes/photo.routes.js
+++ w8/nasa-photos-backend/routes/photo.routes.js
@@ -1,11 +1,27 @@
 const express = require('express')
+const { isAuthenticated } = require('../middleware/middlewares')
 const Photo = require('../models/Photo.model')
+const Review = require('../models/Review.model')
 const router = express.Router()
 
-/* GET users listing. */
 router.get('/', async (req, res, next) => {
   const photos = await Photo.find()
   res.json({ photos })
+})
+
+router.get('/:id', async (req, res, next) => {
+  const photos = await Photo.findById(req.params.id)
+  res.json({ photos })
+})
+
+router.get('/:id/reviews', async (req, res, next) => {
+  const reviews = await Review.find({ photo: req.params.id })
+  res.json({ reviews })
+})
+
+router.post('/:id/reviews', isAuthenticated, async (req, res, next) => {
+  const review = await Review.create({ photo: req.params.id, ...req.body })
+  res.json({ review })
 })
 
 module.exports = router
