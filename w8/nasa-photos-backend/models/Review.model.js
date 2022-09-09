cdf532cdcff710f3a264922549dde9d59bccbf67
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-09 17:20:52 +0200

    review model

---
+++ w8/nasa-photos-backend/models/Review.model.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose')
+const Photo = require('./Photo.model')
+const User = require('./User.model')
+
+const reviewSchema = new Schema({
+  photo: {
+    type: Schema.Types.ObjectId,
+    ref: Photo,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: User,
+  },
+  content: {
+    type: Schema.Types.String,
+  },
+  rating: {
+    type: Schema.Types.Number,
+    min: 1,
+    max: 5,
+    validate: {
+      validator: (value) => Number.isInteger(value),
+      message: ({ value }) => `${value} is not an integer value`,
+    },
+  },
+})
+
+const Review = model('Review', reviewSchema)
+
+module.exports = Review
