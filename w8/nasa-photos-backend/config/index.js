83062f0abbda6416c6d175f082cae4db2c723d21
Robin James Kerrison <7150842+rjkerrison@users.noreply.github.com>
2022-09-12 13:03:35 +0200

    cors

--- w8/nasa-photos-backend/config/index.js
+++ w8/nasa-photos-backend/config/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 module.exports = (app) => {
   app.use(logger('dev'))
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: process.env.ORIGIN?.split(',') || 'http://localhost:3001',
+    })
+  )
+
   app.use(express.json())
   app.use(express.urlencoded({ extended: false }))
   app.use(cookieParser())
